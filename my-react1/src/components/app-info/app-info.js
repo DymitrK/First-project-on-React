4e03a80df36fb179662733bb2e6f1aa5f60413e6
DymitrK <dymitrkudlaj@gmail.com>
2021-11-13 20:41:37 +0100

    structure of project was created + app-info

---
+++ my-react1/src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in company</h1>
+            <h2>Total number of employees </h2>
+            <h2>Bonus will receive </h2>
+
+
+        </div>
+    )
+}
+export default AppInfo;
