41bf468481fb45ae2e9ba7f35d09b34b608a8107
DymitrK <dymitrkudlaj@gmail.com>
2021-11-14 16:21:57 +0100

    add employees-add-form + list-item + list

--- my-react1/src/components/app/app.js
+++ my-react1/src/components/app/app.js
@@ -1,7 +1,8 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
-import EmployeesListItem from '../employees-list-item/employees-list-item';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
@@ -16,8 +17,10 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
+
+            <EmployeesList/>
         
-        
+            <EmployeesAddForm/>
         </div>
     );
 }
